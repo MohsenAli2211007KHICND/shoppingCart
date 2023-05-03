607b471a31db7ffed957c2d9a456e709b8da9583
MohsenAli2211007KHICND <mohsen.ali@xloopdigital.com>
2023-05-03 17:38:45 +0500

    Added jar file

--- src/main/java/gul/cart/Cart.java
+++ src/main/java/gul/cart/Cart.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Data; 
 import lombok.NoArgsConstructor;
 
 @Data
